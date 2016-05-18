494a10d46bdeb49cbca773aff281bdce5ae3126d
Federico Mena Quintero <federico@gnome.org>
2016-05-18 07:43:49 -0500

    TileProvider: I hadn't committed this

---
+++ tile_provider.py
@@ -0,0 +1,37 @@
+import requests
+
+class TileProvider:
+    def get_tile_png (self, z, x, y):
+        pass
+
+    def get_tile_size (self):
+        pass
+
+class MapboxTileProvider (TileProvider):
+    def __init__ (self, access_token, username, style_id):
+        self.access_token = access_token
+        self.username = username
+        self.style_id = style_id
+
+    def get_uri_for_tile (self, z, x, y):
+        uri = "https://api.mapbox.com/styles/v1/{username}/{style_id}/tiles/{z}/{x}/{y}".format (
+            username = self.username,
+            style_id = self.style_id,
+            z = z,
+            x = x,
+            y = y)
+
+        return uri
+
+    def make_request_for_tile (self, z, x, y):
+        url = self.get_uri_for_tile (z, x, y)
+        r = requests.get (url,
+                          params = { 'access_token' : self.access_token })
+        return r
+
+    def get_tile_png (self, z, x, y):
+        r = make_request_for_tile (self, z, x, y)
+        return r.content
+
+    def get_tile_size (self):
+        return 512
