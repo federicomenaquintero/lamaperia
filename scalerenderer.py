91a718ea6c67d5ffc0d69ff1fad7f4edc69a9b24
Federico Mena Quintero <federico@gnome.org>
2016-05-21 17:35:06 -0500

    ScaleRenderer - parametrize the full kilometers and the small-scale divisions

--- scalerenderer.py
+++ scalerenderer.py
@@ -1,5 +1,7 @@
 import cairo
 from cairoutils import *
+from gi.repository import Pango
+from gi.repository import PangoCairo
 
 def inch_to_mm (inch):
     return inch * 25.4
@@ -14,12 +16,14 @@ def pt_to_mm (pt):
     return pt / 72.0 * 25.4
 
 class ScaleRenderer:
-    def __init__ (self, map_scale_denom):
+    def __init__ (self, map_scale_denom, kilometers_total, kilometers_with_small_scale, small_scale_meters):
         self.map_scale_denom = map_scale_denom
         self.outline_thickness_pt = 0.5
         self.color_rgb = (0, 0, 0)
         self.rule_width_mm = 2.5
-        self.kilometers_to_show = 5
+        self.kilometers_total = 5
+        self.kilometers_with_small_scale = kilometers_with_small_scale
+        self.small_scale_meters = small_scale_meters
 
     def render_alternate_divisions (self, cr, num_divisions, left, top, division_length, division_height):
         half_height = division_height / 2.0
@@ -39,9 +43,9 @@ class ScaleRenderer:
     # and the vertical top of the scale rule.
     #
     def render (self, cr, center_x, top_y):
-        millimeters_to_show = self.kilometers_to_show * 1000000
-        rule_length_mm = millimeters_to_show / self.map_scale_denom
-        mm_per_kilometer = rule_length_mm / self.kilometers_to_show
+        millimeters_total = self.kilometers_total * 1000000
+        rule_length_mm = millimeters_total / self.map_scale_denom
+        mm_per_kilometer = rule_length_mm / self.kilometers_total
 
         # upper-left coords will be (leftmost_x, top_y)
         # size of rule will be (rule_length_mm, self.rule_width_mm)
@@ -62,15 +66,15 @@ class ScaleRenderer:
 
         # Paint the main divisions on the right
         self.render_alternate_divisions (cr,
-                                         self.kilometers_to_show - 1,
-                                         leftmost_x + mm_per_kilometer, top_y,
+                                         self.kilometers_total - self.kilometers_with_small_scale,
+                                         leftmost_x + mm_per_kilometer * self.kilometers_with_small_scale, top_y,
                                          mm_per_kilometer, self.rule_width_mm)
 
-        # Paint the 1/10 divisions on the left
+        # Paint the small-scale divisions on the left
         self.render_alternate_divisions (cr,
-                                         10,
+                                         self.kilometers_with_small_scale * 1000 // self.small_scale_meters,
                                          leftmost_x, top_y,
-                                         mm_per_kilometer / 10, self.rule_width_mm)
+                                         mm_per_kilometer * self.small_scale_meters / (self.kilometers_with_small_scale * 1000), self.rule_width_mm)
 
 if __name__ == "__main__":
     surface = cairo.PDFSurface ("scale.pdf", mm_to_pt (inch_to_mm (11.0)), mm_to_pt (inch_to_mm (8.5)))
@@ -79,7 +83,19 @@ if __name__ == "__main__":
     factor = mm_to_pt (1.0)
     cr.scale (factor, factor)
 
-    scale_renderer = ScaleRenderer (50000)
+    scale_renderer = ScaleRenderer (50000, 5, 1, 100)
     scale_renderer.render (cr, inch_to_mm (5.5), inch_to_mm (4))
+
+    layout = PangoCairo.create_layout (cr)
+    layout.set_font_description (Pango.font_description_from_string ("Luxi Serif 6"))
+    layout.set_text ("Hola mundo", -1)
+
+    cr.move_to (inch_to_mm (5.5), inch_to_mm (4))
+    cr.scale (pt_to_mm (1), pt_to_mm (1))
+
+    (ink_rect, logical_rect) = layout.get_pixel_extents ()
+    (x, y) = cr.get_current_point ()
+    cr.move_to (x - logical_rect.width / 2, y - logical_rect.height)
+    PangoCairo.show_layout (cr, layout)
 
     surface.show_page ()
