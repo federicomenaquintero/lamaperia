955ac374693aefd054c50b16de8fc234a3145ed9
Federico Mena Quintero <federico@gnome.org>
2016-05-24 08:34:05 -0500

    MapLayout: parse paper size

--- maplayout.py
+++ maplayout.py
@@ -3,17 +3,19 @@ from parsedegrees import *
 from units import *
 import testutils
 
-letter_paper_size_numeric = """
-{
-  "paper-width" : 279.4,
-  "paper-height" : 215.9
-}
-"""
-
 class MapLayout:
     def __init__ (self):
         self.paper_width_mm = inch_to_mm (11)
         self.paper_height_mm = inch_to_mm (8.5)
+
+    def parse_json (self, str):
+        parsed = json.loads (str)
+
+        if "paper-width" in parsed:
+            self.paper_width_mm = parsed["paper-width"]
+
+        if "paper-height" in parsed:
+            self.paper_height_mm = parsed["paper-height"]
 
 class TestMapLayout (testutils.TestCaseHelper):
     def test_map_layout_has_us_letter_default_paper_size (self):
@@ -21,3 +23,17 @@ class TestMapLayout (testutils.TestCaseHelper):
 
         self.assertFloatEquals (layout.paper_width_mm, inch_to_mm (11.0))
         self.assertFloatEquals (layout.paper_height_mm, inch_to_mm (8.5))
+
+    def test_map_layout_parses_paper_size (self):
+        paper_size_numeric = """
+{
+  "paper-width" : 50.8,
+  "paper-height" : 25.4
+}
+"""
+
+        layout = MapLayout ()
+        layout.parse_json (paper_size_numeric)
+
+        self.assertFloatEquals (layout.paper_width_mm, 50.8)
+        self.assertFloatEquals (layout.paper_height_mm, 25.4)
