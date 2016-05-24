bf319579b91168e867054c18a105312fbdf77d1e
Federico Mena Quintero <federico@gnome.org>
2016-05-24 09:43:34 -0500

    MapLayout: parse arbitrary unit strings for paper-width and paper-height

--- maplayout.py
+++ maplayout.py
@@ -12,10 +12,12 @@ class MapLayout:
         parsed = json.loads (str)
 
         if "paper-width" in parsed:
-            self.paper_width_mm = parsed["paper-width"]
+            self.paper_width_mm = parse_units (parsed["paper-width"])
 
         if "paper-height" in parsed:
-            self.paper_height_mm = parsed["paper-height"]
+            self.paper_height_mm = parse_units (parsed["paper-height"])
+
+#################### tests ####################
 
 class TestMapLayout (testutils.TestCaseHelper):
     def test_map_layout_has_us_letter_default_paper_size (self):
@@ -37,3 +39,17 @@ class TestMapLayout (testutils.TestCaseHelper):
 
         self.assertFloatEquals (layout.paper_width_mm, 50.8)
         self.assertFloatEquals (layout.paper_height_mm, 25.4)
+
+    def test_map_layout_parses_inches_paper_size (self):
+        paper_size_numeric = """
+{
+  "paper-width" : "11 in",
+  "paper-height" : "8.5 in"
+}
+"""
+
+        layout = MapLayout ()
+        layout.parse_json (paper_size_numeric)
+
+        self.assertFloatEquals (layout.paper_width_mm, inch_to_mm (11))
+        self.assertFloatEquals (layout.paper_height_mm, inch_to_mm (8.5))
