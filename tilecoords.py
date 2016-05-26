fd6d2ca5a2d859ffe4b8993cd958c00b7c4f499e
Federico Mena Quintero <federico@gnome.org>
2016-05-26 07:35:26 -0500

    tilecoords: Add tests for coordinate conversions

--- tilecoords.py
+++ tilecoords.py
@@ -1,4 +1,5 @@
 import math
+import testutils
 
 # stolen from https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
 #
@@ -31,3 +32,30 @@ def compute_real_world_mm_per_tile (latitude, zoom):
 
     mm_per_tile = meridian_length / tiles_around_the_earth
     return mm_per_tile
+
+#################### tests ####################
+
+class TestTileCoords (testutils.TestCaseHelper):
+    def test_tile_number_and_fraction_roundtrips_to_coordinates (self):
+        start_lat = 19.4621106
+        start_lon = -96.9040473
+
+        for zoom in range (0, 20):
+            (tile_x, tile_y) = coordinates_to_tile_and_fraction (zoom, start_lat, start_lon)
+            (end_lat, end_lon) = tile_number_to_coordinates (zoom, tile_x, tile_y)
+
+            self.assertFloatEquals (start_lat, end_lat)
+            self.assertFloatEquals (start_lon, end_lon)
+
+    def test_cerro_malinche_is_in_the_correct_tile (self):
+        lat = 19.4621106
+        lon = -96.9040473
+        zoom = 15
+
+        expected_tile_x = 7563
+        expected_tile_y = 14577
+
+        (tile_x, tile_y) = coordinates_to_tile_number (zoom, lat, lon)
+
+        self.assertFloatEquals (tile_x, expected_tile_x)
+        self.assertFloatEquals (tile_y, expected_tile_y)
